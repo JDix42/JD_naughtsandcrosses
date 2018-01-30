6b7acc8087eb57fd4b2c64caf7a63f8893d54c28
James Dix <james.dix@postgrad.manchester.ac.uk>
2018-01-30 13:00:17 +0000

    Added comments to Board and a check win method
    Added comment to go through what is happening in the code.
    
    Added three "checkWin" methods. One for a horizontal, one for a vertical and one for a diagonal win. These modifications have been compiled but yet to be tested.

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,15 @@
 public class Board {
 
+//3x3 game board array. Initially this is populated with zeros
 	int[][] arr = 	{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
+//Game constructor
 	public static void main(String[] args) {
-	//int[][] arr = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
-//System.out.println(java.util.Arrays.deepToString(arr));
-//System.out.print(arr[0][0]);
+
 	}
 
+// Method to show how the values are populated across the board. A "1" corresponds
+// to a "X" and a "2" corresponds to a "O".
 	public void reportValues() {
 	
 	System.out.print(arr[0][0] + "|" + arr[0][1] + "|" + arr[0][2] + "\n");
@@ -17,6 +19,7 @@ public class Board {
 	System.out.print(arr[2][0] + "|" + arr[2][1] + "|" + arr[2][2] + "\n");
 }
 
+// Method to report where the "O" and "X" are on the game board.
 	public void reportGame() {
 	
 	for(int i = 0; i < 3; i++) {
@@ -39,6 +42,78 @@ public class Board {
 		System.out.print("-+-+-" + "\n") ; }
 	}
 	}
+
+// Method to if there are three in a row and return what number won.
+// Returning "1" means the "X" win and returning "2" means the "O" win.
+
+// This check is for a horizontal winner
+	public int checkWinHor() {
+	
+	int win = 0;
+	
+	if (arr[0][0] == 1 & arr[0][1] == 1 & arr[0][2] == 1) {
+		win = 1;}
+	else if( arr[0][0] == 2 & arr[0][1] == 2 & arr[0][2] == 2) {
+		win = 2;}
+	else if ( arr[1][0] == 1 & arr[1][1] == 1 & arr[1][2] == 1) {
+		win = 1;}
+	else if ( arr[1][0] == 2 & arr[1][1] == 2 & arr[1][2] == 2) {
+		win = 2;}
+	else if ( arr[2][0] == 1 & arr[2][1] == 1 & arr[2][2] == 1) {
+		win = 1;}
+	else if ( arr[2][0] == 2 & arr[2][1] == 2 & arr[2][2] == 2) {
+		win = 2;}
+	else {
+		win = 0;}
+
+	return win;
+	
+	}
+
+// This check is for a vertical winner
+	public int checkWinVer() {
+	
+	int win = 0;
+	
+	if (arr[0][0] == 1 & arr[1][0] == 1 & arr[2][0] == 1) {
+		win = 1;}
+	else if( arr[0][0] == 2 & arr[1][0] == 2 & arr[2][0] == 2) {
+		win = 2;}
+	else if ( arr[0][1] == 1 & arr[1][1] == 1 & arr[2][1] == 1) {
+		win = 1;}
+	else if ( arr[0][1] == 2 & arr[1][1] == 2 & arr[2][1] == 2) {
+		win = 2;}
+	else if ( arr[0][2] == 1 & arr[1][2] == 1 & arr[2][2] == 1) {
+		win = 1;}
+	else if ( arr[0][2] == 2 & arr[1][2] == 2 & arr[2][2] == 2) {
+		win = 2;}
+	else {
+		win = 0;}
+
+	return win;
+	
+	}
+
+// This check is for a diagonal winner
+	public int checkWinDia() {
+	
+	int win = 0;
+	
+	if (arr[0][0] == 1 & arr[1][1] == 1 & arr[2][2] == 1) {
+		win = 1;}
+	else if( arr[0][0] == 2 & arr[1][1] == 2 & arr[2][2] == 2) {
+		win = 2;}
+	else if ( arr[0][2] == 1 & arr[1][1] == 1 & arr[2][0] == 1) {
+		win = 1;}
+	else if ( arr[0][2] == 2 & arr[1][1] == 2 & arr[2][0] == 2) {
+		win = 2;}
+	else {
+		win = 0;}
+
+	return win;
+	
+	}
+		
 
 }
 
