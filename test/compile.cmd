75cdb345ebe59b9d6f128454e7378ad2b0cc07d2
James Dix <james.dix@postgrad.manchester.ac.uk>
2018-02-01 12:22:24 +0000

    Compile script
    This file is used to compile all of the java files for the test classes.
    
    It is run in Windows command prompt by "cmd /C compile.cmd"

---
+++ test/compile.cmd
@@ -0,0 +1,7 @@
+javac -classpath ../src/;../test/ ../src/Board.java
+
+javac -classpath ../src/;../test/ ../test/testBoardFun.java
+
+javac -classpath ../src/;../test/ ../test/testBoard.java
+
+java -classpath ../src/;../test/ testBoard
